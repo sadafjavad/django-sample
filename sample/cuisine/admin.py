476437d284537d5bd43c81d90c6f9a05eb4d88f7
sadaf <sadafjavad@gmail.com>
2025-12-01 22:13:42 +0000

    MY FIRST WEB APP

--- sample/cuisine/admin.py
+++ sample/cuisine/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Cuisine
+
+class CuisineAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author','created','publish','status')
+    list_filter = ('status', 'created', 'publish', 'author')
+    search_fields = ('name', 'desc')
+    prepopulated_fields = {'slug': ('name',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
+admin.site.register(Cuisine, CuisineAdmin)
